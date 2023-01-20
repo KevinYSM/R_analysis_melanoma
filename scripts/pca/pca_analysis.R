742a21759eb5a63937135cef6335c3b8dd410f51
KevinYSM <kevinshahm11@gmail.com>
2023-01-20 12:40:02 +0800

    minor changes

--- scripts/pca/pca_analysis.R
+++ scripts/pca/pca_analysis.R
@@ -1,7 +1,22 @@
 #https://cran.r-project.org/web/packages/ggfortify/vignettes/plot_pca.html
 
 library("ggfortify")
-gene_counts_df<-get_RPKM_normalised_data()
+gene_counts_df<-open_gene_counts()
 oncogenes_df<-get_oncogenes_df()
-pca_res<-prcomp(gene_counts_df, scale. = TRUE)
-autoplot(pca_res, data=gene_counts_df, colour='col')
+pca_res<-prcomp(oncogenes_df, scale. = TRUE)
+autoplot(pca_res)
+autoplot(pca_res, data=oncogenes_df,  label=TRUE,label.size = 3, loadings=TRUE)
+
+
+#with eigen vectors
+autoplot(pca_res, data=oncogenes_df,  label=TRUE,label.size = 3, loadings=TRUE)
+
+
+#plotting K-means
+set.seed(1)
+autoplot(kmeans(oncogenes_df, 5), data = oncogenes_df)
+
+
+
+rm(list=ls())
+
